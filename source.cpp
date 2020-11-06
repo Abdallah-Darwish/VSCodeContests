e9a521944e494f35cfe1aca5f09accb6912e27e5
abdallahesam <abdallahesam@hotmail.com>
2020-11-07 01:43:24 +0200

    Finally included _DEBUG_ in clangd flags

--- source.cpp
+++ source.cpp
@@ -1,15 +1,15 @@
 #include <algorithm>
+#include <cmath>
 #include <fstream>
-#include <iostream>
+#include <limits>
 #include <sstream>
+#include <iostream>
 using namespace std;
-using ll	= signed long long int;
-const int N = 1e6;
+using ll	= long long int;
+const int N = 1e5 + 10;
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cout.tie(nullptr);
-	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false), cout.tie(nullptr), cin.tie(nullptr);
 #ifdef _DEBUG_
 	ifstream in("in.txt");
 	streambuf *cinbuf = cin.rdbuf();
